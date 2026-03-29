22a5436c4cc349193c0a1a300cede8e8e025022f
itsryosuke <155581789+itsryosuke@users.noreply.github.com>
2026-03-29 13:03:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Template Repository
+
+This repository is going to serve as a template for my future projects.
---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+});
